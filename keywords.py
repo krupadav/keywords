056f5f5a3a6f3ae7961f0bb9ac30bd04b942f8d5
krupadav <107058808+krupadav@users.noreply.github.com>
2022-08-15 19:47:41 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,9 @@
+"""
+'False','True' 'None','if','else','elif','while','for','continue','break','pass'
+'and','or','not', 'as','in','is','import','return'
+'class','def','global','nonlocal','lambda'
+'assert','async','await','del','except','finally','from','raise', 'try', 'with', 'yield'
+"""
+print('using "print(help(keyword))" we can get the details of the keyword')
+a = help(False)
+print(a)
